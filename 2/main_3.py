3adf6c2a5461e282134be78c2e133f1e5db5dd0f
miladyousefi <yousefimiladdd@gmail.com>
2021-04-15 19:57:40 +0430

    add main_3

--- 2/main_3.py
+++ 2/main_3.py
@@ -1,8 +1,7 @@
-print("*")
-print("*")
-print("*")
-print("*")
-print("*")
-print("*")
-print("*")
-print("***********")
+str=""
+for row in range(0,7):
+    for column in range(0,7):
+        if(column==1 or (row)):
+            str=str+"*\n"
+print(str)
+        
